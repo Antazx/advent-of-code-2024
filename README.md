b705c414711b0bbb436f45594fdd8e85b6832057
Guillermo Anta <guillermo.anta@telecomboutique.com>
2024-12-01 20:52:40 +0100

    Update README.md and remove log

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Advent of code 2024
 
+## How to run
+
+To run the solutions, you need to install the dependencies and run the following command:
+
+```
+tsx day-x/index.ts
+```
+
 These are my solutions for the advent of code 2024
+
+## --- Day 1: Trebuchet?! ---
+
+- [Solution 1](/day-1/part-1/trebuchet-1.ts) ✅
+- [Solution 2](/day-1/part-2/trebuchet-2.ts) ✅
--- utils/quicksort.ts
+++ utils/quicksort.ts
@@ -1,22 +1,19 @@
 export function quicksort(list: number[]): number[] {
-    if (list.length <= 1) {
-        return list;
+  if (list.length <= 1) {
+    return list;
+  }
+
+  const pivot = list[list.length - 1];
+  const leftArr: number[] = [];
+  const rightArr: number[] = [];
+
+  for (let i = 0; i < list.length - 1; i++) {
+    if (list[i] < pivot) {
+      leftArr.push(list[i]);
+    } else {
+      rightArr.push(list[i]);
     }
+  }
 
-    const pivot = list[list.length - 1];
-    const leftArr: number[] = [];
-    const rightArr: number[] = [];
-
-    for (let i = 0; i < list.length - 1; i++) {
-        if (list[i] < pivot) {
-            leftArr.push(list[i]);
-        } else {
-            rightArr.push(list[i]);
-        }
-    }
-
-    return [...quicksort(leftArr), pivot, ...quicksort(rightArr)];
+  return [...quicksort(leftArr), pivot, ...quicksort(rightArr)];
 }
-
-const unsortedArray = [5, 1, 4, 2, 7, 3, 6];
-console.log(quicksort(unsortedArray));
